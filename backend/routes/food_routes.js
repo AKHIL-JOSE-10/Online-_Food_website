84b15455c5d7aa8d13db965e41852ea8210fafca
Akhil-jose-10 <akhiljosetcr@gmail.com@gmail.com>
2024-07-10 15:47:12 +0530

    image upload

--- backend/routes/food_routes.js
+++ backend/routes/food_routes.js
@@ -8,7 +8,7 @@ const router = express.Router()
 
 const storage = multer.diskStorage({
     destination : ( req , file , callback) =>{
-        callback(null , 'foods/images')
+        callback(null , 'public/images')
     } , 
         filename : (req,file,cb)=>{
             cb(null , file.fieldname + "_" +Date.now() + path.extname(file.originalname))
